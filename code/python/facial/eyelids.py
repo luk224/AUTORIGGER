c4e45ba7daa45b9048d00399077a49d793859446
luk224 <luking1995@gmail.com>
2019-08-03 12:37:47 +0200

    a

--- code/python/facial/eyelids.py
+++ code/python/facial/eyelids.py
@@ -1,4 +1,5 @@
 sele = cmds.ls(sl=1)
+print cmds.getAttr(sele[0]+'.controlPoints[0]')
 print len(sele)
 
 def baseName(name):
@@ -36,10 +37,20 @@ locs = cmds.group (em =1,  n ='{0}_{1}_{2}_locsGrp'.format(side,name,zone))
 jnts = cmds.group(em=1, n ='{0}_{1}_{2}_jntsGrp'.format(side,name,zone))
 rig = cmds.group(em =1, n ='{0}_{1}_{2}_rig'.format(side,name,zone))
 ctls = cmds.group (em =1,  n ='{0}_{1}_{2}_ctlsGrp'.format(side,name,zone))
-clusters = cmds.group (em =1,  n ='{0}_{1}_{2}_clustersGrp'.format(side,name,zone))
+curvJoints = cmds.group (em =1,  n ='{0}_{1}_{2}_jntsCurve_grp'.format(side,name,zone))
 
+"""
+cmds.xform(locs,ws=1,t = centerPos)
+cmds.xform(jnts,ws=1,t = centerPos)
+cmds.xform(rig,ws=1,t = centerPos)
+cmds.xform(ctls,ws=1,t = centerPos)
+cmds.xform(curvJoints,ws=1,t = centerPos)
+
+"""
 cmds.parent(locs, jnts, rig)
 
+
+    
 for i in range (len(vtxs)):
     cmds.select(cl =1)
     posCv = cmds.xform(vtxs[i], q =1, ws =1, t =1)
@@ -50,11 +61,13 @@ for i in range (len(vtxs)):
     cmds.parent(jntCenter,jnts)
     
     loc= cmds.spaceLocator(n = '{0}_{1}_{2}_{3}_loc'.format(side,name,zone,str(i) ))[0]
+    
+    
     ndCurInfo = cmds.createNode('pointOnCurveInfo', n = curva.replace('curve','nd'))
     
     cmds.connectAttr(curva + '.worldSpace[0]',ndCurInfo+'.inputCurve')
     cmds.setAttr(ndCurInfo +'.parameter', i)
-    cmds.connectAttr(ndCurInfo+'.position', loc+'.translate')    
+    ##cmds.connectAttr(ndCurInfo+'.controlPoints['+str(i)+']', loc+'.translate')    
     cmds.parent(loc, locs)
     cmds.select(loc, jntCenter)
     cnsAim = cmds.aimConstraint(offset = (0,0,0), w=1, aimVector=(1,0,0), upVector = (0,1,0), worldUpType ='objectrotation',worldUpVector = (0,1,0),worldUpObject = upperLoc)
@@ -65,8 +78,10 @@ baseName(curva)
 ctlCurve = cmds.duplicate(curva, n= baseName(curva)+'_ctlCurve', rr=1)[0]
 cmds.rebuildCurve(ctlCurve, ch=0, rpo=1, rt=0, end=1, kr=0, kcp=0, kep=1, kt=0, s=2, d=3, n= ctlCurve)
 cmds.delete(ctlCurve, ch=1)
-
+cmds.pointPosition(curva+'.ep[1]', w=1)
+cmds.select(ctlCurve)
 print 'adjust '+ctlCurve+' manually...'
+
 cmds.wire(curva,w = ctlCurve)
 
 #CREATE CLUSTERS to control de ctlCurve.
